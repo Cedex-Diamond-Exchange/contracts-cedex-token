865c808604c1cf76bab9bb19386841a11759f292
Boris Idzinsky <borisi@techfinancials.com>
2017-12-27 14:47:52 +0200

    Add distribute tokens function to Cedex contract

--- contracts/Cedex.sol
+++ contracts/Cedex.sol
@@ -8,12 +8,15 @@ import "./BurnableToken.sol";
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `StandardToken` functions.
  */
-contract Cedex is BurnableToken {
+contract Cedex is BurnableToken, Ownable {
 
 	string public constant name = "CEDEX";
 	string public constant symbol = "CEDEX";
 	uint8 public constant decimals = 18;
 	uint256 public constant INITIAL_SUPPLY = 100000000 * 10**18;
+  uint public constant defaultTransferAllowedDate = 1517443200;
+
+  mapping(address => uint) public transferAllowedDates;
 
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
@@ -22,6 +25,21 @@ contract Cedex is BurnableToken {
   	totalSupply = INITIAL_SUPPLY;
     balances[msg.sender] = INITIAL_SUPPLY;
     Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+  }
+
+  function transfer(address _to, uint _value) returns (bool) {
+    require(now > transferAllowedDates[msg.sender]);
+    return super.transfer(_to, _value);
+  }
+  
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+      require(now > transferAllowedDates[_from]);
+      return super.transferFrom(_from, _to, _value);
+  }
+
+  function distributeToken(address _to, uint _value, uint _transferAllowedDate) onlyOwner {
+    transferAllowedDates[_to] = _transferAllowedDate;
+    super.transfer(_to, _value);
   }
 
 }
